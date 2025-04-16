a0704bf656677a50fe4adcf6f92b947700e95974
mihaimaganu17 <maganu_gx_mihai@yahoo.com>
2025-04-16 10:10:32 +0200

    Reader and reading basic types

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 mod transformer;
+mod read;
 
 use transformer::Transformer;
 
---
+++ src/read.rs
@@ -0,0 +1,34 @@
+pub struct Reader {
+    data: Vec<u8>,
+    pos: usize,
+}
+
+impl Reader {
+    fn from_file(path: &str) -> Self {
+        let data = std::fs::read(path).expect("Failed to read file");
+        Self {
+            data,
+            pos: 0,
+        }
+    }
+
+    fn read_i32(&mut self) -> Result<i32, Error> {
+        let mut buffer = [0; 4];
+        buffer.copy_from_slice(self.data.get(self.pos..self.pos + 4)
+            .ok_or(Error::InvalidPosition(self.pos))?
+        );
+        Ok(i32::from_le_bytes(buffer))
+    }
+
+    fn read_u32(&mut self) -> Result<u32, Error> {
+        let mut buffer = [0; 4];
+        buffer.copy_from_slice(self.data.get(self.pos..self.pos + 4)
+            .ok_or(Error::InvalidPosition(self.pos))?
+        );
+        Ok(u32::from_le_bytes(buffer))
+    }
+}
+
+pub enum Error {
+    InvalidPosition(usize),
+}
